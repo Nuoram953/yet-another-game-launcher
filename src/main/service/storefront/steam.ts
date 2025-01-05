5e17fc183ebeb865f66632f714516d0720cb6792
Nuoram953 <antoinemaroun@gmail.com>
2025-01-05 17:23:50 -0500

    Cleanup

--- src/main/service/storefront/steam.ts
+++ src/main/service/storefront/steam.ts
@@ -24,13 +24,11 @@ class Steam implements Storefront {
   }
 
   async getOwnedGames() {
-    //api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=4C42F094130F3F920F815DA092FE65A2&steamid=76561198091857715&format=json&include_appinfo=1
-
     const response = await axios.get(
       "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001",
       {
         params: {
-          steamid: "76561198091857715",
+          steamid: this.steamid,
           key: this.apiKey,
           format: "json",
           include_appinfo: 1,
