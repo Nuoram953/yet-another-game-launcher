410ca4548ba5b1a0b49e2fc4a9f56d2b3123edcd
Nuoram953 <antoinemaroun@gmail.com>
2025-07-01 18:21:53 -0400

    refactor: move sidebar to sheet component
    refactor: move sidebar to sheet component
    
    refactor: move sidebar to sheet component
    
    refactor: move sidebar to sheet component

--- src/renderer/components/layout/Layout.tsx
+++ src/renderer/components/layout/Layout.tsx
@@ -1,12 +1,12 @@
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@render//components/ui/sidebar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Separator } from "@radix-ui/react-separator";
-import { AppSidebar } from "@render/components/sidebar/AppSidebar";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "../ui/breadcrumb";
 import { useBreadcrumbsContext } from "@render//context/BreadcrumbsContext";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../button/Button";
 import { House, SidebarIcon } from "lucide-react";
+import { Sidebar } from "../sidebar/Sidebar";
+import { CookieType, getCookie, setCookie } from "@render/utils/cookieUtil";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -14,7 +14,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   const { breadcrumbs, setBreadcrumbs } = useBreadcrumbsContext();
-  const [open, setOpen] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(Boolean(getCookie(CookieType.IS_SIDEBAR_OPEN, "boolean")) || false);
   const navigate = useNavigate();
 
   const handleClickBreadcrumbs = (path: string) => {
@@ -25,47 +25,45 @@ export default function Layout({ children }: LayoutProps) {
     navigate(path, { replace: true });
   };
 
-  return (
-    <SidebarProvider open={open} onOpenChange={setOpen}>
-      <div className="flex h-screen w-screen overflow-hidden">
-        <AppSidebar />
-        <SidebarInset className="flex min-h-0 flex-1 flex-col">
-          <main className="flex min-h-0 flex-1 flex-col bg-gradient-to-br from-gray-900 to-gray-800">
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b border-gray-800 bg-sidebar">
-              <div className="flex items-center gap-2 px-4">
-                <Button
-                  icon={SidebarIcon}
-                  intent="icon"
-                  size="fit"
-                  onClick={() => {
-                    setOpen(!open);
-                  }}
-                />
-                {breadcrumbs.length > 0 && (
-                  <Button icon={House} intent="icon" size="fit" onClick={() => navigate("/")} />
-                )}
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumb>
-                  <BreadcrumbList>
-                    {breadcrumbs.map((breadcrumb, index) => (
-                      <React.Fragment key={breadcrumb.path}>
-                        <BreadcrumbItem className="hidden md:block">
-                          <BreadcrumbLink onClick={() => handleClickBreadcrumbs(breadcrumb.path)}>
-                            {breadcrumb.label}
-                          </BreadcrumbLink>
-                        </BreadcrumbItem>
-                        {index !== breadcrumbs.length - 1 && <BreadcrumbSeparator className="hidden md:block" />}
-                      </React.Fragment>
-                    ))}
-                  </BreadcrumbList>
-                </Breadcrumb>
-              </div>
-            </header>
+  useEffect(() => {
+    setCookie(CookieType.IS_SIDEBAR_OPEN, open);
+  }, [open]);
 
-            <div className="">{children}</div>
-          </main>
-        </SidebarInset>
-      </div>
-    </SidebarProvider>
+  return (
+    <div className="flex h-screen w-screen overflow-hidden">
+      <Sidebar open={open} setOpen={setOpen} />
+      <main className="flex min-h-0 flex-1 flex-col bg-gradient-to-br from-gray-900 to-gray-800">
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b border-gray-800 bg-sidebar">
+          <div className="flex items-center gap-2 px-4">
+            <Button
+              icon={SidebarIcon}
+              intent="icon"
+              size="fit"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            />
+            {breadcrumbs.length > 0 && <Button icon={House} intent="icon" size="fit" onClick={() => navigate("/")} />}
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                {breadcrumbs.map((breadcrumb, index) => (
+                  <React.Fragment key={breadcrumb.path}>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink onClick={() => handleClickBreadcrumbs(breadcrumb.path)}>
+                        {breadcrumb.label}
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    {index !== breadcrumbs.length - 1 && <BreadcrumbSeparator className="hidden md:block" />}
+                  </React.Fragment>
+                ))}
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        <div className="">{children}</div>
+      </main>
+    </div>
   );
 }
